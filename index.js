dfb1a14fe943e574e80e6744304d1c770ce9a0a5
Akerke <muiskar.demon@gmail.com>
2024-11-11 19:05:16 +0500

    Task.B

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
-let ai= +prompt("Ай атауын шығару: 1-ден 12-ге дейін сан енгізіңіз")
+// let ai= +prompt("Ай атауын шығару: 1-ден 12-ге дейін сан енгізіңіз")
 
-if(ai<=5 && ai>=3){
-    alert(ai + " Spring")
-} 
-else if(ai<=8 && ai>=6){
-    alert(ai + " Summer")
+// if(ai<=5 && ai>=3){
+//     alert(ai + " Spring")
+// } 
+// else if(ai<=8 && ai>=6){
+//     alert(ai + " Summer")
+// }
+// else if(ai<=11 && ai>=9){
+//     alert(ai + " Autumn")
+// }
+// else if((ai<=2 && ai>=0) || ai==12){
+//     alert(ai + " Winter")
+// }
+// else{
+//     alert("1 жыл 12 айдан тұрады 1 - ден 12- ге дейін сан еңгіз")
+// }
+
+let a = +prompt("Бірінші санды енгізіңіз")
+let b = +prompt("Екінші санды енгізіңіз")
+let orta = (a + b) / 2
+
+if(a>b){
+    alert("a саны b санынан үлкен")
 }
-else if(ai<=11 && ai>=9){
-    alert(ai + " Autumn")
-}
-else if((ai<=2 && ai>=0) || ai==12){
-    alert(ai + " Winter")
+else if(a<b){
+    alert("a саны b санынан кіші")
 }
 else{
-    alert("1 жыл 12 айдан тұрады 1 - ден 12- ге дейін сан еңгіз")
+    alert("Сандар тең")
 }
+
+alert("Орташа мәні: " + orta)
